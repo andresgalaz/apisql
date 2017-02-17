48b1b7eb51b4de5baa6772573009bc6024496e6f
Andres Galaz <andres.galaz@gmail.com>
2017-02-17 11:11:05 -0300

    Utilitario para resetear eventos. Creación de daños para siniestros

---
+++ vSiniestroDano.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW vSiniestroDano AS
+  SELECT sd.pSiniestro, sd.pTpDano, d.cNombre cTpDano
+       , case when sd.bExiste = '1' then 'X' else '' end cPresente
+  FROM tSiniestroDano sd
+  INNER JOIN tTpDano d on d.pTpDano = sd.pTpDano
