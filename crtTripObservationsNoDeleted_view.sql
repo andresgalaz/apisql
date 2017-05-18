9aede52b16117032fb7af3b0d6457c2f7dff6d85
Andres Galaz <andres.galaz@gmail.com>
2017-05-18 18:07:30 -0300

    Se agrega observations deleted

---
+++ crtTripObservationsNoDeleted_view.sql
@@ -0,0 +1,5 @@
+﻿drop view if exists trip_observations_no_deleted_view;
+create view trip_observations_no_deleted_view as
+select o.*
+from trip_observations_g  o
+where not exists ( select 1 from trip_observations_deleted d where d.id = o.id );
--- crtTripObservationsView_7puentes.sql
+++ crtTripObservationsView_7puentes.sql
@@ -6,23 +6,15 @@ select t.id                     AS trip_id          , t.client_id              A
      , c.driver_id              AS driver_id        , o.prefix_observation     AS prefix
      , r.points                 AS puntos           , o.observed_value         AS obs_value
      , o.permited_value         AS permited_value   , o.from_time              AS obs_fecha
-     , ( case
-           when isnull(so.display_name) then so.name
-           else so.display_name
-         end )                  AS calle
-     , ( case
-           when isnull(st.display_name) then st.name
-           else st.display_name
-         end
-       )                        AS calle_inicio
+     , ( case when isnull(so.display_name) then so.name else so.display_name end ) AS calle
+     , ( case when isnull(st.display_name) then st.name else st.display_name end ) AS calle_inicio
      , d.latitude               AS latitude         , d.longitude              AS longitude
      , t.updated_at             AS ts_modif 
   from trips t
-       join      clients c               on c.id            = t.client_id
-       left join trip_observations_g o   on o.trip_id       = t.id
---     left join trip_observations   o   on o.trip_id       = t.id
-       left join observation_ranges r    on r.id            = o.observation_range_id
-       left join streets so              on so.id           = o.street_id
-       left join streets st              on st.id           = t.main_street_id
-       left join trip_details d          on d.trip_id       = t.id 
-                                        and d.event_date    = o.from_time
+       join      clients c                           on c.id            = t.client_id
+       left join trip_observations_no_deleted_view o on o.trip_id       = t.id
+       left join observation_ranges r                on r.id            = o.observation_range_id
+       left join streets so                          on so.id           = o.street_id
+       left join streets st                          on st.id           = t.main_street_id
+       left join trip_details d                      on d.trip_id       = t.id 
+                                                    and d.event_date    = o.from_time
