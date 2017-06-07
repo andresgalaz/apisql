4f29c3b46bec9327e8fe31fb565151f7134717b9
Andres Galaz <andres.galaz@gmail.com>
2017-06-07 14:46:45 +0000

    v2

---
+++ v2/prCreaTmpScoreVehiculo.sql
@@ -0,0 +1,24 @@
+﻿DELIMITER //
+DROP PROCEDURE IF EXISTS prCreaTmpScoreVehiculo //
+CREATE PROCEDURE prCreaTmpScoreVehiculo ()
+BEGIN
+
+	-- Tabla temporal de resultados por usuario y vehiculo
+	CREATE TEMPORARY TABLE IF NOT EXISTS wMemoryScoreVehiculo (
+		pVehiculo				INTEGER		UNSIGNED	NOT NULL,
+		nKms					DECIMAL(10,2)	UNSIGNED	NOT NULL	DEFAULT '0.0',
+		nScore					DECIMAL(10,2)	UNSIGNED	NOT NULL	DEFAULT '0.0',
+		nQViajes				INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		nQFrenada				INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		nQAceleracion			INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		nQVelocidad				INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		nQCurva					INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		nDescuento				DECIMAL( 5,2)				NOT NULL	DEFAULT '0.0',
+		nDiasTotal				INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		nDiasUso				INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		nDiasPunta				INTEGER		UNSIGNED	NOT NULL	DEFAULT '0',
+		PRIMARY KEY (pVehiculo)
+	) ENGINE=MEMORY;
+	DELETE FROM wMemoryScoreVehiculo;
+
+END //
---
+++ v2/vViaje.sql
@@ -0,0 +1,25 @@
+﻿DROP VIEW IF EXISTS vViaje;
+CREATE VIEW vViaje AS
+SELECT	v.pVehiculo			AS	fVehiculo		,	v.cPatente	AS	cPatente
+	 ,	v.fUsuarioTitular	AS	fUsuarioTitular
+	 ,	ut.cNombre			AS	cNombreTitular
+	 ,	ini.fUsuario		AS	fUsuario
+	 ,	IFNULL(uu.cNombre,'Desconocido')			AS	cNombreConductor
+	 ,	ini.nIdViaje		AS	nIdViaje
+	 ,	ini.cCalle			AS	cCalleInicio	,	fin.cCalle	AS	cCalleFin
+	 ,	ini.tEvento			AS	tInicio			,	fin.tEvento	AS	tFin
+	 ,	ini.nValor			AS	nScore			,	fin.nValor	AS	nKms
+FROM	tParamCalculo				AS	prm
+		-- Inicio del Viaje
+		INNER JOIN tEvento			AS	ini ON	ini.fTpEvento	=	1 
+		-- Fin del Viaje
+		INNER JOIN tEvento			AS	fin	ON	fin.nIdViaje	=	ini.nIdViaje
+									       AND	fin.fTpEvento   =	2 -- Fin del Viaje
+										   AND	fin.nValor		> 	prm.nDistanciaMin
+		INNER JOIN tVehiculo		AS 	v	ON	v.pVehiculo		= 	ini.fVehiculo
+		-- Solo muestra los viajes de los usuario relacionados. Pueden existir viajes de usuario no identificados
+		INNER JOIN tUsuarioVehiculo AS	uv	ON	uv.pVehiculo	= 	ini.fVehiculo
+										   AND	uv.pUsuario		=	ini.fUsuario
+		INNER JOIN tUsuario			AS	ut	ON	ut.pUsuario		=	v.fUsuarioTitular
+		LEFT JOIN  tUsuario			AS	uu	ON	uu.pUsuario		=	ini.fUsuario
+ORDER BY ini.tEvento DESC
