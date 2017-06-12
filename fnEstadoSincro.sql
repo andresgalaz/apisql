6764369e02b95d5680e3b17dc36c80730108621f
Andres Galaz <andres.galaz@gmail.com>
2017-06-12 17:25:10 -0300

    Estado Sincro

---
+++ fnEstadoSincro.sql
@@ -0,0 +1,13 @@
+﻿DELIMITER //
+DROP FUNCTION IF EXISTS fnEstadoSincro //
+CREATE FUNCTION fnEstadoSincro(prm_tUltimo TIMESTAMP) RETURNS VARCHAR(20)
+BEGIN
+	DECLARE nDias INTEGER DEFAULT DATEDIFF( DATE(NOW()), DATE(prm_tUltimo));
+	IF nDias < 2 THEN
+		RETURN 'Actualizado';
+	END IF;
+	IF nDias < 6 THEN
+		RETURN 'Desactualizado';
+	END IF;
+	RETURN 'Critico'
+END //
