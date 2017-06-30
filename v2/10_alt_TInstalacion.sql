c9ebaaf88b13f4fba2c80c43c9a4368120bd3459
Andres Galaz <andres.galaz@gmail.com>
2017-06-30 13:09:41 -0300

    Instalacion

---
+++ v2/10_alt_TInstalacion.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `score`.`tInstalacion` 
+CHANGE COLUMN `cOpcion` `cAccion` VARCHAR(20) NOT NULL ;
+ALTER TABLE `score`.`tInstalacion` 
+CHANGE COLUMN `cEstado` `cEstado` VARCHAR(20) NULL ;
