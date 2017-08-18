22b128c45192ca4575b49b8fe162a2fdb183529f
Andres Galaz <andres.galaz@gmail.com>
2017-08-18 18:15:41 -0300

    hlp facturador

--- hlpFacturador.sql
+++ hlpFacturador.sql
@@ -1,25 +1,23 @@
 
+drop table wMemoryScoreVehiculo;
+drop table wMemoryScoreVehiculoCount;
 call prFacturador(null);
-select 'Real', v.cPatente, v.dIniVigencia, u.cNombre, u.cEmail
-        , w.nScore,w.nKms,w.nKmsPond
-        , w.nDescuentoKM,w.nDescuentoSinUso,w.nDescuentoPunta
-        , w.nDescuentoKM+w.nDescuentoSinUso+w.nDescuentoPunta nDescuentoSinPond
-        , w.nDescuento
-        , w.nDiasTotal, w.nDiasUso, w.nDiasPunta, w.nDiasSinMedicion
+select 'Real' cTpCalculo, v.cPatente, v.dIniVigencia, u.cEmail, u.pUsuario, u.cNombre, w.pVehiculo, w.dInicio, (w.dFin + INTERVAL -1 DAY ) dFin, w.nKms, w.nKmsPond, w.nScore
+     , w.nDescuentoKM, w.nDescuentoSinUso, w.nDescuentoPunta
+     , w.nDescuentoKM + w.nDescuentoSinUso + w.nDescuentoPunta as nDescSinPonderar, w.nDescuento
+     , w.nQViajes, w.nQFrenada, w.nQAceleracion, w.nQVelocidad, w.nQCurva, w.nDiasTotal, w.nDiasUso, w.nDiasPunta, w.nDiasSinMedicion, w.tUltimoViaje, w.tUltimaSincro
 from wMemoryScoreVehiculo w
-		left join tVehiculo v on v.pVehiculo = w.pVehiculo
-        left join tUsuario u on u.pUsuario = v.fUsuarioTitular
+join tVehiculo v on v.pVehiculo = w.pVehiculo
+join tUsuario  u on u.pUsuario = v.fUsuarioTitular
 union all
-select 'Sin Multa', v.cPatente, v.dIniVigencia, u.cNombre, u.cEmail
-        , w.nScore,w.nKms,w.nKmsPond
-        , w.nDescuentoKM,w.nDescuentoSinUso,w.nDescuentoPunta
-        , w.nDescuentoKM+w.nDescuentoSinUso+w.nDescuentoPunta nDescuentoSinPond
-        , w.nDescuento
-        , w.nDiasTotal, w.nDiasUso, w.nDiasPunta, w.nDiasSinMedicion
+select 'Sin multa' cTpCalculo, v.cPatente, v.dIniVigencia, u.cEmail, u.pUsuario, u.cNombre, w.pVehiculo, w.dInicio, (w.dFin + INTERVAL -1 DAY ) dFin, w.nKms, w.nKmsPond, w.nScore
+     , w.nDescuentoKM, w.nDescuentoSinUso, w.nDescuentoPunta
+     , w.nDescuentoKM + w.nDescuentoSinUso + w.nDescuentoPunta as nDescSinPonderar, w.nDescuento
+     , w.nQViajes, w.nQFrenada, w.nQAceleracion, w.nQVelocidad, w.nQCurva, w.nDiasTotal, w.nDiasUso, w.nDiasPunta, w.nDiasSinMedicion, w.tUltimoViaje, w.tUltimaSincro
 from wMemoryScoreVehiculoSinMulta w
-		left join tVehiculo v on v.pVehiculo = w.pVehiculo
-        left join tUsuario u on u.pUsuario = v.fUsuarioTitular;
--- where v.dIniVigencia <= w.dInicio;
+join tVehiculo v on v.pVehiculo = w.pVehiculo
+join tUsuario  u on u.pUsuario = v.fUsuarioTitular
+order by 2,1; 
 
 SELECT	v.pVehiculo				, v.cPatente				, v.cIdDispositivo			, v.bVigente				,
 		v.fTpDispositivo		, v.fCuenta					, v.fUsuarioTitular			, v.tModif					,
