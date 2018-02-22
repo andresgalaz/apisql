c346a6903cbde59e1b96ff068c8fb4effbf1f0c5
Andres Galaz <andres.galaz@gmail.com>
2018-02-22 09:28:37 -0300

    ahorro

--- hlpAhorroPesos.sql
+++ hlpAhorroPesos.sql
@@ -28,17 +28,36 @@ AND m.nro_patente in ('AB686YD','AA467BP','EPZ791')
 order by m.NRO_PATENTE,m.pMovim;
 -- GROUP BY m.nro_patente, m.PORCENT_DESCUENTO ;
 
-select
-AB686YD, 2.596,
-AA467BP, 3.411,
-EPZ791, 1.617,
-KXZ633, 2.987,
-EXM369, 1.566,
-KZI628, 1.789,
-MRW848, 1.578,
-AB844YD, 2.221,
-LTQ105, 2.093,
-GOZ716, 1.429,
-NXL561, 2.063;
+drop table ahorro;
 
- 
+create table ahorro as 
+SELECT m.pMovim, m.NOMBRE, m.APELLIDO, m.NRO_PATENTE, m.FECHA_EMISION,year(m.fecha_inicio_vig) `año`, monthname(m.fecha_inicio_vig) mes, m.fecha_inicio_vig, m.CODENDOSO, m.DESC_ENDOSO
+     , m.PORCENT_DESCUENTO descuento
+     , m.sumaaseg
+     , m.prima
+     , m.derechoemi, m.impsellados
+     , (m.derechoemi + m.ingbrutos + m.impsellados + m.iva + m.impvarios+m.impinternos + m.tasasuper + m.servsoc) suma_imp
+     , m.premio
+     , round(100 * ( m.prima - 47.83 ) / 1.2325) / 100  primaTecnica
+     , 1/ ( 1 - m.PORCENT_DESCUENTO/100 )   descInv
+FROM integrity.tMovim m
+where m.premio > 0 and m.PORCENT_DESCUENTO is not null
+-- AND m.nro_patente in ('AB686YD','AA467BP','EPZ791')
+order by m.NRO_PATENTE,m.pMovim;
+
+SELECT a.pMovim, concat(a.NOMBRE,' ', a.APELLIDO) nombre, a.NRO_PATENTE patente, a.FECHA_EMISION
+     , a.descuento
+     , a.sumaaseg
+     , a.prima
+     , a.derechoemi, a.impsellados
+     , a.suma_imp
+     , round(10000*a.suma_imp/prima ) / 100 porc_imp
+     , a.prima+ a.suma_imp premioCalc
+     , a.premio
+     , a.primaTecnica
+     , ( a.primaTecnica * descInv )   primaTecnicaSinDesc
+     , round(a.prima * descInv ) primaSinDesc
+	 , round(a.premio * descInv)  premioSinDesc
+	 , round(a.premio * descInv) - a.premio ahorro
+from ahorro a
+where a.descuento = 0;
