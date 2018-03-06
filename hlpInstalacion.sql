f41b19c4c799970512e662dd56ad3bdb4e2dede8
Andres Galaz <andres.galaz@gmail.com>
2018-03-05 22:57:08 -0300

    mac

--- hlpInstalacion.sql
+++ hlpInstalacion.sql
@@ -3,7 +3,7 @@ from tInstalacion i
 left join tUsuario u on u.pUsuario = i.fUsuario
 left join tVehiculo v on v.pVehiculo = i.fVehiculo
 left join tUsuario c on c.pUsuario = v.fUsuarioTitular
-where i.tModif >= date( '2017-10-01' )
+where i.tModif >= date( '2018-02-01' )
 group by i.fVehiculo, i.cPatente, i.fUsuario, u.cNombre 
 order by dInstalacion desc
 ;
