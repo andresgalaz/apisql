97a234bcad101034befce8553aef808af96eab55
Andres Galaz <andres.galaz@gmail.com>
2018-03-09 17:27:19 -0300

    Prorroga anulada

--- vFacturaProrroga.sql
+++ vFacturaProrroga.sql
@@ -10,10 +10,7 @@ PT * ( 1.2325036803 ) + 47.83 = prima
 DROP VIEW vFacturaProrroga;
 CREATE VIEW vFacturaProrroga AS
 SELECT	  m.pMovim
--- 		, u.cEmail
--- 		, u.cNombre
 		, m.NRO_PATENTE								cPatente
--- 		, v.dIniVigencia
 		, m.FECHA_EMISION							dEmision
 		, m.fecha_inicio_vig						dInicio
 		, m.fecha_vencimiento						dFin
@@ -44,10 +41,16 @@ SELECT	  m.pMovim
 		          + m.impvarios+m.impinternos 
 		          + m.tasasuper + m.servsoc ) / m.prima ))							nAhorro
 FROM	integrity.tMovim m
--- 		INNER JOIN tVehiculo v	ON v.cPatente = m.nro_patente AND v.bVigente = '1'
--- 		INNER JOIN tUsuario u	ON u.pUsuario = v.fUsuarioTitular
 WHERE	m.premio > 0 AND m.porcent_descuento IS NOT NULL
 AND     m.codEndoso = '9900'
+-- Si hay mas de una prorroga para el mismo periodo solo se considera la última, esto porque
+-- seguramente las otras son anulaciones
+AND		m.pMovim in	  (	SELECT	max(dup.pMovim)
+						FROM	integrity.tMovim dup
+						WHERE	dup.premio > 0 AND dup.porcent_descuento IS NOT NULL
+						AND     dup.codEndoso = '9900'
+						GROUP BY dup.NRO_PATENTE,dup.FECHA_INICIO_VIG,dup.FECHA_VENCIMIENTO
+					  )
 ;
 SELECT f.* FROM score.vFacturaProrroga f order by f.cPatente, f.dEmision
 ;
