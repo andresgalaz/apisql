f1bc55533d7dfe99db34efe2f257685e98585846
Andres Galaz <andres.galaz@gmail.com>
2018-06-27 14:05:24 -0300

    migraViajes v2.0a

--- snapcar.tripsView.sql
+++ snapcar.tripsView.sql
@@ -1,15 +1,16 @@
 USE snapcar;
 DROP VIEW  IF EXISTS snapcar.trips_view;
 CREATE VIEW snapcar.trips_view as
-SELECT t.id							AS trip_id		, c.driver_id					AS driver_id	, c.vehicle_id					AS vehicle_id
-	 , t.updated_at					AS updated_at	, round((t.distance / 1000),4)	AS distance		, t.from_date - interval 3 hour	AS from_date
-     , t.to_date - interval 3 hour	AS to_date		
+SELECT t.id										AS trip_id		, t.updated_at								AS updated_at
+     , convert(c.driver_id, unsigned integer)	AS driver_id	, convert(c.vehicle_id, unsigned integer)	AS vehicle_id
+     , t.from_date - interval 3 hour			AS from_date    , t.to_date - interval 3 hour				AS to_date		
+     , round((t.distance / 1000),4)				AS distance		
+     , dIni.latitude 							AS latitude_ini	, dIni.longitude							AS longitude_ini
+     , dFin.latitude							AS latitude_fin , dFin.longitude							AS longitude_fin
      , snapcar.fnNombreCalle( 'L', st.`name`, st.street_number, st.town,st.city, st.substate, st.state,st.country )	AS calle_inicio
      , snapcar.fnNombreCalle( 'C', st.`name`, st.street_number, st.town,st.city, st.substate, st.state,st.country )	AS calle_inicio_corta
      , snapcar.fnNombreCalle( 'L', se.`name`, se.street_number, se.town,se.city, se.substate, se.state,se.country )	AS calle_fin
      , snapcar.fnNombreCalle( 'C', se.`name`, se.street_number, se.town,se.city, se.substate, se.state,se.country )	AS calle_fin_corta
-     , dIni.latitude 				AS latitude_ini	, dIni.longitude				AS longitude_ini, dFin.latitude					AS latitude_fin
-     , dFin.longitude				AS longitude_fin
 FROM   trips t
 	   INNER JOIN	clients			c		ON c.id = t.client_id
 	   LEFT JOIN	trip_details	dIni	ON dIni.trip_id = t.id
